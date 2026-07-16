a14dac88496b680386ce74e61b282cd6d4554fbd
Ewan Chamberlain <ec719@sussex.ac.uk>
2026-07-16 17:12:27 +0100

    Add Fortran program example and update .gitignore for VSCode

---
+++ section1.01/my_exercise1.f90
@@ -0,0 +1,9 @@
+program exercise1
+    use iso_fortran_env, only : output_unit, error_unit, input_unit
+
+    write (output_unit, *) output_unit
+    write (output_unit, *) input_unit
+    write (output_unit, *) error_unit
+    
+
+end program exercise1
